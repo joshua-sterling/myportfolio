606ecc464627e4ed29087597b7714d14157b2d60
jsterling <joshua.sterling15@gmail.com>
2024-03-12 10:07:57 -0500

    Add EF tools and create initial migration

---
+++ myportfolio.Server/Migrations/20240312150630_InitialCreate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myportfolio.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "RowingEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Distance = table.Column<int>(type: "int", nullable: false),
+                    TotalTime = table.Column<TimeSpan>(type: "time(6)", nullable: false),
+                    EventDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    StrokeRate = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RowingEvents", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RowingEvents");
+        }
+    }
+}
